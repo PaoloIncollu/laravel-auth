c86b4da4b3e6d62a2a1704c42ea288c6a5a4fa06
PaoloIncollu <paoloinco@gmail.com>
2024-10-25 17:42:05 +0200

    aggiunte rotte home page

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 // Controllers
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\Admin\MainController as AdminMainController;
-
+use App\Http\Controllers\Admin\ProjectController as AdminProjectController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -25,7 +25,7 @@ Route::prefix('admin')
     ->group(function () {
 
     Route::get('/dashboard', [AdminMainController::class, 'dashboard'])->name('dashboard');
-
+    Route::get('/homePage', [AdminProjectController::class, 'dashboard'])->name('homePage');
 });
 
 require __DIR__.'/auth.php';
